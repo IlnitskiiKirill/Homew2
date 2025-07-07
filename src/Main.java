601e840d7e1c201ee16dead2d933f68c971c749b
hello word <ilnitskiykirill@gmail.com>
2025-07-07 21:41:26 +0300

    ДЗ №2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,87 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+            System.out.println("Задание №1");
+
+            var dog = 8.0;
+            var cat = 3.6;
+            var paper = 763789;
+
+            System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper="+paper);
+
+        System.out.println("Задание №2");
+
+        dog =dog +4;
+        System.out.println("dog=" + dog);
+        cat=cat+4;
+        System.out.println("cat=" + cat);
+        paper=paper+4;
+        System.out.println("paper="+paper);
+
+        System.out.println("Задание №3");
+
+        dog =dog-3.5;
+        System.out.println("dog=" + dog);
+        cat=cat-1.6;
+        System.out.println("cat=" + cat);
+        paper=paper-7639;
+        System.out.println("paper="+paper);
+
+        System.out.println("Задание №4");
+
+        var friend =19;
+        System.out.println("friend=" + friend);
+       friend=friend+2;
+        System.out.println("friend=" + friend);
+        friend=friend/7;
+        System.out.println("friend=" + friend);
+
+        System.out.println("Задание №5");
+
+        var frog =3.5;
+        System.out.println("frog=" + frog);
+        frog=frog*10;
+        System.out.println("frog=" + frog);
+        frog=frog/3.5;
+        System.out.println("frog=" + frog);
+        frog=frog+4;
+        System.out.println("frog=" + frog);
+
+        System.out.println("Задание №6");
+
+        var firstboxer =78.2;
+        System.out.println("first boxer=" + firstboxer + "кг");
+        var secondboxer=82.7;
+        System.out.println("second boxer=" + secondboxer + "кг");
+        var result =firstboxer+secondboxer;
+        System.out.println("all boxers=" + result + "кг");
+        var result2=secondboxer-firstboxer;
+        System.out.println("difference=" + result2 + "кг");
+
+        System.out.println("Задание №7");
+
+        var result3 =secondboxer%firstboxer;
+        System.out.println("result=" + result3);
+
+        System.out.println("Задание №8");
+
+        var alltime = 640;
+        var daytime=8;
+        var officeman = alltime/daytime;
+        System.out.println("всего сотрудников в компании=" + officeman + "человек");
+
+        var officeman2=officeman+94;
+
+        var alltime2=officeman2*8;
+        System.out.println("если в офисе работает 174 сотрудника, то всего " + alltime2 + "часов работы может быть поделено между ними");
+
+
+
         }
     }
-}
+
